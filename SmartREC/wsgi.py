6e1a1109874ad56301ffe5f82ff03a06a6d47c3c
darshan761 <darshanpatil761@gmail.com>
2019-10-28 19:11:18 +0530

    first commit

---
+++ SmartREC/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SmartREC project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmartREC.settings')
+
+application = get_wsgi_application()
